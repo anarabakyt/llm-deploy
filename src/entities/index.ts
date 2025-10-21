d326584541b40b4eb199eb29742e9301d1a43c9d
Anara Akmatalieva <73492945+anarabakyt@users.noreply.github.com>
2025-10-21 10:17:45 +0300

    Update index.ts
    аа

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -3,4 +3,4 @@ export type {Model} from './Model';
 export type {Chat} from './Chat';
 export type {Message, MessageAuthor} from './Message';
 export type {ModelResponse} from './ModelResponse';
-export type {DialogBranch, BranchTree, BranchData} from './DialogBranch';
+export type { BranchTree, BranchData} from './DialogBranch';
